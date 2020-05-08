e175a51d21cb922302abbb8dc45b34013223597c
Samuel Kushnir <samkushnir@gmail.com>
2020-05-08 15:41:58 -0400

    small text changes

--- organization-auth/src/Components/SignupComponent.js
+++ organization-auth/src/Components/SignupComponent.js
@@ -38,27 +38,16 @@ function SignupComponent() {
           </Typography>
                 <form className={classes.form} noValidate>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
+                        <Grid item xs={12} sm={12}>
                             <TextField
                                 autoComplete="fname"
-                                name="firstName"
+                                name="orgName"
                                 variant="outlined"
                                 required
                                 fullWidth
-                                id="firstName"
-                                label="First Name"
+                                id="orgName"
+                                label="Organization Name"
                                 autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
                             />
                         </Grid>
                         <Grid item xs={12}>

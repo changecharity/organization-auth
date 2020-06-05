4fb9ac9051dd5697af2972a8e9931a022620e0e6
Samuel Kushnir <samkushnir@gmail.com>
2020-06-05 19:17:54 -0400

    updates to link bank account

--- organization-auth/src/Components/SignupComponent.js
+++ organization-auth/src/Components/SignupComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, makeStyles, ThemeProvider, Container } from '@material-ui/core'
+import React, { useCallback } from 'react';
+import { Avatar, Button,Chip, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, makeStyles, ThemeProvider, Container } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import theme from '../Theme'
 import axios from 'axios'
 import {
@@ -8,7 +9,8 @@ import {
     Switch,
     Route,
   } from "react-router-dom";
-  import { Link as RouterLink } from "react-router-dom"
+import { Link as RouterLink } from "react-router-dom"
+import { usePlaidLink } from 'react-plaid-link';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -28,13 +30,49 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(3, 0, 2),
     },
+    link: {
+        marginTop:"10px",
+        marginBottom: "-5px"
+    },
+    bankChip: {
+        marginTop:"10px"
+    }
 }));
 
 function SignupComponent(props) {
     const classes = useStyles();
     const [orgName, setOrgName] = React.useState("")
+    const [bankAccountEntered, setBankAccountEntered] = React.useState(false)
     const [email, setEmail] = React.useState("")
     const [pass, setPass] = React.useState("")
+    const [plaidToken, setPlaidToken] = React.useState("")
+    const [ein, setEin] = React.useState("")
+    const [bankName, setBankName] = React.useState("")
+    const [accountId, setAccountId] = React.useState("")
+    const onSuccess = useCallback((token, metadata) => {
+        // send token to server
+        console.log(token)
+        console.log(metadata)
+        console.log(metadata["accounts"])
+        setPlaidToken(token)
+        setBankName(metadata["institution"]["name"])
+        setAccountId(metadata["accounts"][0]["id"])
+        setBankAccountEntered(true)
+      }, []);
+    const config = {
+        clientName: 'Plaid Quickstart',
+        env: 'sandbox',
+        product: ['transactions'],
+        publicKey: '014d4f2c01905eafa07cbcd2755ef5',
+        onSuccess,
+        // ...
+      };
+    const { open, ready, error } = usePlaidLink(config);
+
+    const handleDelete = () => {
+        console.info('You clicked the delete icon.');
+        setBankAccountEntered(false)
+      };
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -44,9 +82,9 @@ function SignupComponent(props) {
             name: orgName,
             email: email,
             password: pass,
-            ein: 12341920,
-            plaid_public_token: "beeud0dniqu9e1",
-            plaid_account_id: "beeuald0soun7ique1"
+            ein: ein,
+            plaid_public_token: plaidToken,
+            plaid_account_id: accountId
         }),
         method: "POST",
         withCredentials: true}).then(response => {
@@ -85,6 +123,20 @@ function SignupComponent(props) {
                                 autoFocus
                             />
                         </Grid>
+                        <Grid item xs={12} sm={12}>
+                            <TextField
+                                autoComplete="fname"
+                                name="orgEin"
+                                variant="outlined"
+                                value={ein}
+                                onChange={e=> setEin(e.target.value)}
+                                required
+                                fullWidth
+                                id="orgEin"
+                                label="Organization EIN"
+                                autoFocus
+                            />
+                        </Grid>
                         <Grid item xs={12}>
                             <TextField
                                 variant="outlined"
@@ -119,6 +171,29 @@ function SignupComponent(props) {
                             />
                         </Grid>
                     </Grid>
+                    {
+                        !bankAccountEntered ? (
+                            <Button
+                                onClick={() => open()}
+                                disabled={!ready}
+                                fullWidth
+                                variant="contained"
+                                color="secondary"
+                                className={classes.link}
+                            >
+                                Link Bank Account
+                            </Button>
+                        ) :
+                        (
+                            <Chip
+                                className={classes.bankChip}
+                                icon={<AccountBalanceIcon />}
+                                label={bankName}
+                                onDelete={handleDelete}
+                                color="secondary"
+                            />
+                        )
+                    }
                     <Button
                         type="submit"
                         fullWidth
